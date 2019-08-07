4d35515152e082d929ee8d8f3f1353d2107c0fd7
dwarfer7634 <wild.woodpecker4203@gmail.com>
2019-08-07 17:28:01 +0900

    7/24

--- python/ch3/simple_cbow.py
+++ python/ch3/simple_cbow.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append('..')
 import numpy as np
-from common.layers import MatMul, SoftmaxWithLoss
+from common.layers import MatMul, SoftmaxWithLoss, Embedding
 
 class SimpleCBOW:
     def __init__(self, vocab_size, hidden_size):
@@ -10,8 +10,11 @@ class SimpleCBOW:
         W_in = 0.01 * np.random.randn(V, H).astype('f')
         W_out = 0.01 * np.random.randn(H, V).astype('f')
 
-        self.in_layer0 = MatMul(W_in)
-        self.in_layer1 = MatMul(W_in)
+        # self.in_layer0 = MatMul(W_in)
+        # self.in_layer1 = MatMul(W_in)
+        # self.out_layer = MatMul(W_out)
+        self.in_layer0 = Embedding(W_in)
+        self.in_layer1 = Embedding(W_in)
         self.out_layer = MatMul(W_out)
         self.loss_layer = SoftmaxWithLoss()
 
@@ -24,10 +27,12 @@ class SimpleCBOW:
         self.word_vecs = W_in
 
     def forward(self, contexts, target):
-        h0 = self.in_layer0.forward(contexts[:, 0])
-        h1 = self.in_layer1.forward(contexts[:, 1])
+        
+        h0 = self.in_layer0.forward(np.argmax(contexts[:, 0], axis=1))
+        h1 = self.in_layer1.forward(np.argmax(contexts[:, 1], axis=1))
         h = (h0 + h1) * 0.5
         score = self.out_layer.forward(h)
+        print(score.shape, target.shape)
         loss = self.loss_layer.forward(score, target)
         return loss
 
@@ -38,3 +43,6 @@ class SimpleCBOW:
         self.in_layer0.backward(da)
         self.in_layer1.backward(da)
         return None
+
+if __name__ == '__main__':
+    cbow = SimpleCBOW(5, 10)
---
+++ python/ch4/negative_sampling_layer.py
@@ -0,0 +1,18 @@
+import sys
+sys.path.append('..')
+import numpy as np
+from common.layers import MatMul, SoftmaxWithLoss, Embedding
+
+class EmbeddingDot:
+    def __init__(self, W):
+        self.embed = Embedding(W)
+        self.params = self.embed.params
+        self.grads = self.embed.grads
+        self.cache = None 
+
+    def forward(self, h, idx):
+        target_W = self.embed.forward(idx)
+        out = np.sum(target_W * h, axis=1)
+
+        self.cache = (h, target_W)
+        return out 
